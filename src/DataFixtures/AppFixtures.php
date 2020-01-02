1c4875cf008b2e15a91089da82141996f6a5852e
faballa <Vous@exemple.com>
2020-01-02 18:59:48 +0000

    premiergit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,12 +2,20 @@
 
 namespace App\DataFixtures;
 
+
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class AppFixtures extends Fixture
-{
+class AppFixtures extends Fixture{
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User ("SuperAdmin");
